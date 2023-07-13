e245587bc539bb96ce0abb82860434cbd982ce5c
klaudilag <klaudilag@wp.pl>
2023-07-13 11:58:06 +0200

    Zmiana nazwy klasy testowej.

--- src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
+++ src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootTest
 class BigMacTestSuite {
 
